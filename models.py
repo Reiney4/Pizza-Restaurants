90cf76bb585061be64ae138d3d14b82092a95fec
Valerie <valeriejeptoo4@gmail.com>
2023-09-25 11:35:45 +0300

    created  models

--- models.py
+++ models.py
@@ -1,10 +1,58 @@
-from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
-def create_app():
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
 
-    app = Flask(__name__)
-
-    # add routes
-    from app import routes
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
     
-    return app
+    serialize_rules = ('-pizzas.restaurant','-restaurant.pizzas')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50),nullable= False,unique= True)
+    address = db.Column(db.String)
+    
+    pizzas = db.relationship('RestaurantPizza',back_populates='pizzas')
+    
+    @validates("name")
+    def validate_name(self, key, name):
+        if  len(name) > 50:
+            raise ValueError("Name must be less than 50 characters")
+        return name
+    
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+    
+    serialize_rules = ('-restaurants.pizza','-restaurant.pizzas')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurants = db.relationship('RestaurantPizza',back_populates='restaurants')
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+    
+    serialize_rules = ('restaurant','pizza')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurants = db.relationship('RestaurantPizza',back_populates='restaurants')
+    pizzas = db.relationship('RestaurantPizza',back_populates='pizzas')
+
+    @validates("price")
+    def validate_price(self, key, value):
+        if not (1 <= value <= 30):
+            raise ValueError("Price must be between 1 and 30")
+        return value
